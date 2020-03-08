8b578d7ea6c67af67d3643f70ca7870077517d95
srish <srishakatux@gmail.com>
2020-03-08 14:55:36 -0700

    clicking on a video from the gallery redirects to another page with video info

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.png';
 import './App.css';
 import VideoGallery from './VideoGallery';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- client/src/VideoGallery.js
+++ client/src/VideoGallery.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Vimeo from '@u-wave/react-vimeo';
 import videoService from './services/videoService';
+import { Button } from 'react-bootstrap';
 
 function VideoGallery() {
   const [videos, setVideos] = useState(null);
+  const [videoObject, setVideoObject] = useState(null);
 
   useEffect(() => {
     if(!videos) {
@@ -20,7 +22,7 @@ function VideoGallery() {
   const renderVideo = video => {
     let id = video.uri.substring(8);
     return (
-      <div key={id} className="video-item">
+      <div key={id} className="video-item" onClick={() => setVideoObject(video)}>
         <Vimeo 
           video={id}
           showTitle={false}
@@ -37,11 +39,40 @@ function VideoGallery() {
 
   return (
     <div className="video-gallery">
-        {(videos && videos.length > 0) ? (
+        {!videoObject &&
+        <div className="videos">
+        {(videos!= null && videos.length > 0) ? (
           videos.map(video => renderVideo(video))
         ) : (
           <p>No videos found</p>
         )}
+        </div>
+        }
+        {videoObject &&
+        <div class="video-page">
+            <Button bsPrefix="super-btn" className="back-button" onClick={() => setVideoObject(null)} variant="primary">Back to Video Gallery</Button>
+            <div class="video-box">
+                <Vimeo
+                video={videoObject.uri.substring(8)}
+                showTitle={false}
+                showPortrait={false}
+                color='fdeaed'
+                showByline={false}
+                controls={false}
+                width={500}
+                />
+            </div>
+            <div class="video-info-box">
+                <h1 className="title">{videoObject.name}</h1>
+                <p className="created">Uploaded on {videoObject.created_time}</p>
+                <h3 className="username">{videoObject.user.name}</h3>
+                <p className="description">{videoObject.description}</p>
+                {(videoObject.tags!= null && videoObject.tags.tag) &&
+                    <span className="tags">{videoObject.tags.tag}</span>
+                }
+            </div>
+        </div>
+        }
     </div>
   );
 }
