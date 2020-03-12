132c17af74711c85e0c20bf6cba8bcd93b7c1a99
srish <srishakatux@gmail.com>
2020-03-11 23:27:35 -0700

    mechanism to show a single video on a separate page with unique url same as video id

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import logo from './logo.png';
 import './App.css';
-import VideoGallery from './VideoGallery';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Header';
+import Routes from './Routes';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <VideoGallery>
-      </VideoGallery>
+      <Header/>
+      <Routes/>
     </div>
   );
 }
---
+++ client/src/Routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Router, Switch, Route} from 'react-router-dom';
+import History from './History';
+import VideoGallery from './VideoGallery';
+import VideoPage from './VideoPage';
+
+function Routes() {
+  return (
+    <Router history={History}>
+      <Switch>
+        <Route path="/" exact component={VideoGallery} />
+        <Route path="/video/:id" component={VideoPage} />
+      </Switch>
+    </Router>
+  );
+}
+export default Routes;
