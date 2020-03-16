4de4bcd4c1839ea5d0d3812e05eead25e052b36a
srish <srishakatux@gmail.com>
2020-03-15 17:20:29 -0700

    placeholder for comments and bootstrapification of video page

--- client/src/VideoPage.js
+++ client/src/VideoPage.js
@@ -3,7 +3,9 @@ import Vimeo from '@u-wave/react-vimeo';
 import {useParams, useLocation} from 'react-router-dom';
 import videoService from './services/videoService';
 import {Button} from 'react-bootstrap';
+import {Row, Container, Col} from 'react-bootstrap';
 import history from './History';
+import Comments from './Comments';
 
 
 function VideoPage() {
@@ -24,41 +26,50 @@ function VideoPage() {
   };
 
   return (
-    <div className="video-page">
-      <Button bsPrefix="super-btn" className="back-button" variant="primary"
-        onClick={() => history.push('/')}>Back to Video Gallery</Button>
-
+    <Container className="video-page">
       {(videoObject == null) &&
       <p>No video found :-(</p>
       }
+      <Row>
+        <Col className="button-box" lg={3} xs={12}>
+          <Button bsPrefix="super-btn" className="back-button" variant="primary"
+            onClick={() => history.push('/')}>Back to Video Gallery</Button>
+        </Col>
+      </Row>
 
       {(videoObject && videoObject !== null) &&
-      <div className="video-box">
-        <Vimeo
-          video={videoObject.uri.substring(8)}
-          showTitle={false}
-          showPortrait={false}
-          color='fdeaed'
-          showByline={false}
-          controls={false}
-          width={500}
-        />
-      </div>
+      <Row>
+        <Col className="video-box" xs={9}>
+          <Vimeo
+            video={videoObject.uri.substring(8)}
+            showTitle={false}
+            showPortrait={false}
+            color='fdeaed'
+            showByline={false}
+            controls={false}
+            width={835}
+          />
+        </Col>
+      </Row>
       }
 
       {(videoObject && videoObject !== null) &&
-      <div className="video-info-box">
-        <h1 className="title">{videoObject.name}</h1>
-        <p className="created">{videoObject.stats.plays} views | Uploaded on
-          {videoObject.created_time}</p>
-        <h3 className="username">{videoObject.user.name}</h3>
-        <p className="description">{videoObject.description}</p>
-        {videoObject.tags.map((item, index) => {
-          return <span key={'tag-' + index} className="tags">{item.tag}</span>;
-        })}
-      </div>
+      <Row>
+        <Col className="video-info-box" xs={9}>
+          <h1 className="title">{videoObject.name}</h1>
+          <p className="created">{videoObject.stats.plays} views | Uploaded on
+            {videoObject.created_time}</p>
+          <h3 className="username">{videoObject.user.name}</h3>
+          <p className="description">{videoObject.description}</p>
+          {videoObject.tags.map((item, index) => {
+            return <span key={'tag-' + index} className="tags">{item.tag}
+            </span>;
+          })}
+          <Comments></Comments>
+        </Col>
+      </Row>
       }
-    </div>
+    </Container>
   );
 }
 
