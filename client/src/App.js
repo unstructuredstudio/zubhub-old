15dbb9c06a4300679c723eaebb240615dc3fbd77
Suchakra Sharma <suchakra@shiftleft.io>
2020-08-21 15:06:39 -0700

    UI refresh for video page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './Header';
 import Routes from './Routes';
+import Footer from './Footer';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Header/>
       <Routes/>
+      <Footer/>
     </div>
   );
 }
