c987bb4bf9459146870696d28c28fd22d1208ab1
srish <srishakatux@gmail.com>
2020-08-23 16:42:31 -0700

    like/unlike feature

--- client/src/VideoGallery.js
+++ client/src/VideoGallery.js
@@ -3,13 +3,16 @@ import videoService from './services/videoService';
 import history from './History';
 import {Row, Container, Col, Dropdown, DropdownButton} from 'react-bootstrap';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faExternalLinkAlt, faEye} from '@fortawesome/free-solid-svg-icons';
-import { AwesomeButton } from "react-awesome-button";
-import "react-awesome-button/dist/styles.css";
-import './VideoGallery.css'
+import {faExternalLinkAlt, faEye, faThumbsUp}
+  from '@fortawesome/free-solid-svg-icons';
+import {AwesomeButton} from 'react-awesome-button';
+import 'react-awesome-button/dist/styles.css';
+import './VideoGallery.css';
+
 
 function VideoGallery() {
   const [videos, setVideos] = useState(null);
+  const [likes, setLikes] = useState(null);
 
   useEffect(() => {
     if (!videos) {
@@ -17,32 +20,86 @@ function VideoGallery() {
     }
   });
 
+  const updateLikesCount = async (videoID, liked) => {
+    const res = await videoService.updateLikesCount(videoID, liked);
+
+    if (likes) {
+      let videoExists = false;
+      likes.forEach((item) => {
+        if (item.videoId === videoID) {
+          item.liked = liked;
+          item.likes = res[0].likes;
+          videoExists = true;
+        }
+      });
+
+      if (!videoExists) {
+        likes.push({
+          videoId: videoID,
+          liked: liked,
+          _id: res[0]._id,
+          likes: res[0].likes,
+        });
+      }
+
+      setLikes([...likes]);
+    }
+  };
+
   const getVideos = async () => {
     const res = await videoService.getAll();
-    setVideos(res);
+    const likesData = res.likes;
+
+    likesData.forEach((item) => {
+      item.liked = false;
+    });
+
+    setLikes([...likesData]);
+    setVideos(res.videos);
   };
 
   const videoBg = {
-    backgroundColor: "black",
-    height: "85%",
-    width: "100%",
-    marginBottom: 10
+    backgroundColor: 'black',
+    height: '85%',
+    width: '100%',
+    marginBottom: 10,
   };
 
   const renderVideo = (video) => {
     const id = video.uri.substring(8);
+
+    const statsData = likes && likes.find((o) => o.videoId === id );
+    const liked = statsData && statsData['liked'] ? statsData['liked'] : false;
+    const numLikes = statsData && statsData['likes'] ? statsData['likes'] : 0;
+
+
     return (
       <div key={id} className="video-item">
         <div style={videoBg}>
-        <iframe title="vimeo-player" className="vimeo-player"
-          src={'https://player.vimeo.com/video/' + id}
-          frameBorder="0"
-        >
-        </iframe>
+          <iframe title="vimeo-player" className="vimeo-player"
+            src={'https://player.vimeo.com/video/' + id}
+            frameBorder="0"
+          >
+          </iframe>
         </div>
         <Row>
+          {/* <Col>
+            <p><AwesomeButton type="primary"><FontAwesomeIcon icon={faEye} />
+            {video.stats.plays} Views</AwesomeButton> </p>
+          </Col> */}
           <Col>
-            <p><AwesomeButton type="primary"><FontAwesomeIcon icon={faEye} /> {video.stats.plays} Views</AwesomeButton> </p>
+            <Row style={{margin: '0px'}}>
+              <p><AwesomeButton type="primary">{numLikes} likes
+              </AwesomeButton> </p>
+              <a onClick={() => {
+                updateLikesCount(id, !liked);
+              }}>
+                <AwesomeButton type="secondary">
+                  <FontAwesomeIcon icon={faThumbsUp} />
+                  {liked ? 'Unlike' : 'Like'}
+                </AwesomeButton>
+              </a>
+            </Row>
           </Col>
           <Col>
             <p className="video-url">
@@ -62,17 +119,19 @@ function VideoGallery() {
 
   return (
     <Container className="video-gallery">
-      <div className="gallery-sub-nav">
-        <div className="sub-title">
-          Videos
-        </div>
-        <div className="sub-title">
-          <DropdownButton id="category-list" title="Category">
-            <Dropdown.Item href="#/action-1">Toys</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Art</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Stories</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Science Project</Dropdown.Item>
-          </DropdownButton>
+      <div className="videos">
+        <div className="gallery-sub-nav">
+          <div className="sub-title">
+            Videos
+          </div>
+          <div className="sub-title">
+            <DropdownButton id="category-list" title="Category">
+              <Dropdown.Item href="#/action-1">Toys</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Art</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Stories</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Science Project</Dropdown.Item>
+            </DropdownButton>
+          </div>
         </div>
       </div>
       <div className="videos">
