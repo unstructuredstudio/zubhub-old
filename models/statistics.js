abf820926cabe8ed7a34f36b3ee4df78ac90eb47
srish <srishakatux@gmail.com>
2020-08-27 18:22:24 -0700

    modify indent

--- models/statistics.js
+++ models/statistics.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const {Schema} = mongoose;
 
 const likesSchema = new Schema({
-	videoId: String,
-	likes: {type: Number, default: 0, min: 0}
+  videoId: String,
+  likes: {type: Number, default: 0, min: 0}
 });
 
 mongoose.model("likes", likesSchema);
