86920dcd8c47ad58fd494fdca1ddee4863c753c8
srish <srishakatux@gmail.com>
2020-08-27 18:22:24 -0700

    modify indent

--- models/comments.js
+++ models/comments.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const commentsSchema = new Schema({
-	videoId: String,
-	comments: [{
-		postedby: String,
-		description: String,
-		postedago: String
-	}]
+  videoId: String,
+  comments: [{
+    postedby: String,
+    description: String,
+    postedago: String
+  }]
 })
 
 mongoose.model('comments', commentsSchema); 
